c742fbca7cecaf32472803d922ad1dce1809e608
Justin Davis <justin_w_davis@yahoo.com>
2018-10-27 16:35:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var kittens = [ 'Milo', 'Otis', 'Garfield' ] //define your array here
 
-function destructivelyAppendKitten( name ) {
-  kittens.push( name )
-}
+function destructivelyAppendKitten( name ) { kittens.push( name ) }
 
-function destructivelyPrependKitten( name ) {
-  kittens.unshift( name )
-}
+function destructivelyPrependKitten( name ) { kittens.unshift( name ) }
+
+function destructivelyRemoveLastKitten() { kittens.pop() }
+
+function destructivelyRemoveFirstKitten() { kittens.shift() }
+
+function appendKitten( name ) { return [ ...kittens, name ]
