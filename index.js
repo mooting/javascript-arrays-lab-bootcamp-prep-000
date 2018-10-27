4eabdd01aa97c7c24726b2cb289c89da024a62d1
Justin Davis <justin_w_davis@yahoo.com>
2018-10-27 16:37:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,5 @@ function destructivelyRemoveFirstKitten() { kittens.shift() }
 function appendKitten( name ) { return [ ...kittens, name ] }
 
 function prependKitten( name ) { return [ name, ...kittens ]}
+
+function removeLastKitten() { return kittens.slice( 0, kittens.length -1 )}
